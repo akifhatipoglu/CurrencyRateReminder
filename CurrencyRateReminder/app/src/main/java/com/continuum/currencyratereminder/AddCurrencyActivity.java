cf99f11c52f4f9ce1eff59011993cce8ed4e677c
Akif Hatipoğlu <akifhatipoglu.tech@gmail.com>
2017-03-19 18:49:03 +0300

    first realtime db insert

--- CurrencyRateReminder/app/src/main/java/com/continuum/currencyratereminder/AddCurrencyActivity.java
+++ CurrencyRateReminder/app/src/main/java/com/continuum/currencyratereminder/AddCurrencyActivity.java
@@ -10,12 +10,15 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import currencyratereminder.continuum.com.currencyratereminder.R;
 
 public class AddCurrencyActivity extends AppCompatActivity {
 
     private static final String TAG = "AddCurrencyActivity";
+    private DatabaseReference mDatabase;
 
     private FirebaseAuth mAuth;
     private Button btnAdd;
@@ -30,6 +33,7 @@ public class AddCurrencyActivity extends AppCompatActivity {
         edtAmount = (EditText) findViewById(R.id.editTextAmount);
 
         mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +51,11 @@ public class AddCurrencyActivity extends AppCompatActivity {
 
     private void addFirebase() {
         Log.d(TAG, "add Firebase:" + mAuth.getCurrentUser().getUid());
+        String key = mDatabase.child(mAuth.getCurrentUser().getUid()).push().getKey();
+        mDatabase.child(mAuth.getCurrentUser().getUid()).child(key).child("CurrencyType").setValue("USD");
+        mDatabase.child(mAuth.getCurrentUser().getUid()).child(key).child("Currency").setValue("3.6");
+        mDatabase.child(mAuth.getCurrentUser().getUid()).child(key).child("Amount").setValue("50000");
     }
-
 }
+
+
