a3aec0f924371c2c422c4713f4a85e6c760d53f9
Akif Hatipoğlu <akifhatipoglu.tech@gmail.com>
2017-03-19 16:29:33 +0300

    add currency screen added for initiation

---
+++ CurrencyRateReminder/app/src/main/java/com/continuum/currencyratereminder/AddCurrencyActivity.java
@@ -0,0 +1,15 @@
+package com.continuum.currencyratereminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import currencyratereminder.continuum.com.currencyratereminder.R;
+
+public class AddCurrencyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_currency);
+    }
+}
